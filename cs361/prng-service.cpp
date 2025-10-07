ce7c0510056801e076f93101942a002c3c6bf7ca
Anthony Pham <152800119+ProvenAP@users.noreply.github.com>
2025-10-06 19:02:05 -0700

    Update prng-service.cpp

--- cs361/prng-service.cpp
+++ cs361/prng-service.cpp
@@ -5,30 +5,25 @@
 #include <chrono>
 
 int main() {
-    // random generator setup
+    
     std::mt19937 gen(std::random_device{}());
-    std::uniform_int_distribution<> dist(0, 100);  // random numbers 0–100
+    std::uniform_int_distribution<> dist(0, 100); 
 
     while (true) {
-        // sleep for 1 second before checking for “run”
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        // open prng-service.txt to see if it says “run”
         std::ifstream in("prng-service.txt");
         std::string line;
         std::getline(in, line);
         in.close();
 
-        // if we got “run”, generate a new random number
         if (line == "run") {
             int rnd = dist(gen);
 
-            // clear the file and write the random number
             std::ofstream out("prng-service.txt", std::ios::trunc);
             out << rnd;
             out.close();
         }
     }
-
     return 0;
 }
